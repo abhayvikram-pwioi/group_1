5e4955d5521c508beaaa9b8fcd88a00e3024b34e
Shreyansh Singh <shreyansh.singh.r06@gmail.com>
2026-05-31 18:08:07 +0530

    Implement product loading from API
    Fetch product data from API and update UI elements.

--- Project_02/JsFiles/checkout.js
+++ Project_02/JsFiles/checkout.js
@@ -1,1 +1,36 @@
 
+
+async function loadProduct(){
+    try{
+
+        let response =
+        await fetch(
+        "https://dummyjson.com/products/1"
+        );
+
+        let data =
+        await response.json();
+
+        document.getElementById(
+        "productImage"
+        ).src = data.thumbnail;
+
+        document.getElementById(
+        "productName"
+        ).innerText = data.title;
+
+        document.getElementById(
+        "productPrice"
+        ).innerText = "₹" + data.price;
+
+    }
+
+    catch(error){
+
+        console.log(error);
+
+    }
+
+}
+
+loadProduct();
