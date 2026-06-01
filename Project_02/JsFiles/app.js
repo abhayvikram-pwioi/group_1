11a3baa0ec5494909d56679c1b8ad3aa7adda8a2
Aman Kumar <amanthecoder001@gmail.com>
2026-06-01 22:23:03 +0530

    added js part for more products

--- Project_02/JsFiles/app.js
+++ Project_02/JsFiles/app.js
@@ -1,33 +1,41 @@
-const filter = document.getElementById("filter-head");
+let allProducts = [];
 
-if (filter) {
-    let allProducts = [];
+async function getProducts() {
 
-    async function getProducts() {
+    const response = await fetch("https://dummyjson.com/products");
 
-        const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
 
-        const data = await response.json();
+    allProducts = data.products;
 
-        allProducts = data.products;
-
+    if (document.getElementById("product-list")) {
         renderProducts(allProducts);
+    }
+
+    if (document.querySelector(".more-products")) {
+        renderSuggestedProducts();
+    }
 
 
-        const quickBtns = document.querySelectorAll(".view-btn");
+    const quickBtns = document.querySelectorAll(".view-btn");
 
-        quickBtns.forEach(btn => {
+    quickBtns.forEach(btn => {
 
-            btn.addEventListener("click", () => {
+        btn.addEventListener("click", () => {
 
-                modal.classList.add("active");
-                document.body.classList.add("modal-open");
-
-            });
+            modal.classList.add("active");
+            document.body.classList.add("modal-open");
 
         });
 
-    }
+    });
+
+}
+getProducts();
+
+const filter = document.getElementById("filter-head");
+
+if (filter) {
 
     function renderProducts(products) {
 
@@ -105,8 +113,6 @@ if (filter) {
 
 
     }
-
-    getProducts();
 
 
     const categoryCheckboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -209,58 +215,50 @@ if (filter) {
     }
 
 
-
-
-
-    document.addEventListener("click", (e) => {
-
-        const btn = e.target.closest(".view-btn");
-
-        if (!btn) return;
-
-        const productId = btn.dataset.id;
-
-        const product = allProducts.find(
-            p => p.id == productId
-        );
-
-        document.getElementById("quick-view-title").textContent = `${product.title}`;
-        document.getElementById("quick-view-des").textContent = `${product.description}`;
-        document.getElementById("brand-quick-view").textContent = `${product.brand}`;
-        const weight = Number(product.weight)*10;
-        document.getElementById("weight-quick-view").textContent = `${weight}g`;
-        document.getElementById("warranty-quick-view").textContent = `${product.warrantyInformation}`;
-        document.getElementById("dimention-quick-view").textContent = `${product.dimensions.width}cm X ${product.dimensions.height}cm X ${product.dimensions.depth}cm`;
-
-       
-    });
-
-
-
-
-    document.addEventListener("click", (e) => {
-
-        const img = e.target.closest("#product-img");
-
-        if (!img) return;
-
-        const productId = Number(img.dataset.id);
-
-        const selectedProduct =
-            allProducts.find(
-                product => product.id === productId
-            );
-
-        localStorage.setItem(
-            "selectedProduct",
-            JSON.stringify(selectedProduct)
-        );
-
-        window.location.href = "product-details-page.html";
-
-    });
-
 }
+
+document.addEventListener("click", (e) => {
+
+    const btn = e.target.closest(".view-btn");
+
+    if (!btn) return;
+
+    const productId = btn.dataset.id;
+
+    const product = allProducts.find(
+        p => p.id == productId
+    );
+
+    document.getElementById("quick-view-title").textContent = `${product.title}`;
+    document.getElementById("quick-view-des").textContent = `${product.description}`;
+    document.getElementById("brand-quick-view").textContent = `${product.brand}`;
+    const weight = Number(product.weight) * 10;
+    document.getElementById("weight-quick-view").textContent = `${weight}g`;
+    document.getElementById("warranty-quick-view").textContent = `${product.warrantyInformation}`;
+    document.getElementById("dimention-quick-view").textContent = `${product.dimensions.width}cm X ${product.dimensions.height}cm X ${product.dimensions.depth}cm`;
+
+
+});
+
+
+document.addEventListener("click", (e) => {
+
+    const img = e.target.closest("#product-img");
+
+    if (!img) return;
+
+    const productId = Number(img.dataset.id);
+
+    const selectedProduct =
+        allProducts.find(
+            product => product.id === productId
+        );
+
+    localStorage.setItem( "selectedProduct",JSON.stringify(selectedProduct));
+
+    window.location.href = "product-details-page.html";
+
+});
 
 
 
@@ -295,40 +293,113 @@ if (savedProduct) {
 
         document.getElementById("product-brand").textContent = `Brand : ${product.brand}`;
         document.getElementById("product-dimensions").textContent = `Dimensions : ${product.dimensions.width}cm X ${product.dimensions.height}cm X ${product.dimensions.depth}cm`;
-        const weight = Number(product.weight)*10;
+        const weight = Number(product.weight) * 10;
         document.getElementById("product-weight").textContent = `Weight : ${weight}g`;
 
-         document.getElementById("brand").textContent = `${product.brand}`;
+        document.getElementById("brand").textContent = `${product.brand}`;
         document.getElementById("weight").textContent = `${weight}g`;
 
         document.getElementById("warranty").textContent = `${product.warrantyInformation}`;
 
-        document.getElementById("product-rating").innerHTML =`${ getStars(product.rating)}`;
+        document.getElementById("product-rating").innerHTML = `${getStars(product.rating)}`;
     }
 
-
-    
 }
 
 
 
-function getStars(rating){
+function getStars(rating) {
 
     let stars = "";
 
     const fullStars = Math.floor(rating);
 
-    for(let i=0;i<fullStars;i++){
+    for (let i = 0; i < fullStars; i++) {
 
         stars +=
-        `<i class="fa-solid fa-star"></i>`;
+            `<i class="fa-solid fa-star"></i>`;
     }
 
-    for(let i=fullStars;i<5;i++){
+    for (let i = fullStars; i < 5; i++) {
 
         stars +=
-        `<i class="fa-regular fa-star"></i>`;
+            `<i class="fa-regular fa-star"></i>`;
     }
 
     return stars;
+}
+
+
+function renderSuggestedProducts() {
+
+    const currentProduct =JSON.parse(localStorage.getItem("selectedProduct") );
+
+    const suggestedProducts = allProducts .filter(
+                product =>
+                    product.category === currentProduct.category &&
+                    product.id !== currentProduct.id
+            ).slice(0, 4);
+
+
+    const moreProductsList = document.querySelector(".more-products");
+
+
+    moreProductsList.innerHTML = "";
+
+    suggestedProducts.forEach(product => {
+
+        let price = (product.price * 10).toFixed(0);
+
+        moreProductsList.innerHTML += `
+     
+      <div class="product-card" >
+
+                        <div class="product-img-box">
+                            <img src="${product.thumbnail}" alt="book" id="product-img"  data-id="${product.id}">
+
+                            <button class="product-card-wishlist">
+                                <i class="fa-regular fa-heart"></i>
+                            </button>
+                        </div>
+
+
+                        <div class="product-content">
+
+                            <span class="product-category">
+                                   ${product.category}
+                            </span>
+
+                            <h2>
+                                ${product.title}
+                            </h2>
+
+                            <div class="product-card-rating">
+                                   ${getStars(product.rating)}
+                                <span id="rating-count">(200)</span>
+                            </div>
+
+                            <h3>₹${price}</h3>
+
+
+                            <div class="product-card-btns">
+
+                                <button class="add-btn">
+                                    <i class="fa-solid fa-basket-shopping"></i> Add
+                                </button>
+
+                                <button class="view-btn" data-id = "${product.id}">
+                                    <i class="fa-solid fa-eye"></i> Quick View
+                                </button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    
+     `
+
+
+    });
+
 }
