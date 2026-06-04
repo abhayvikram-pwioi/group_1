a6d279281a05fda2575faa1f7e90e7bb29f5f7a1
Shreyansh Singh <shreyansh.singh.r06@gmail.com>
2026-06-04 23:10:34 +0530

    Update checkout.js

--- Project_02/JsFiles/checkout.js
+++ Project_02/JsFiles/checkout.js
@@ -1,42 +1,3 @@
-
-
-async function loadProduct(){
-    try{
-
-        let response =
-        await fetch(
-        "https://dummyjson.com/products/1"
-        );
-
-        let data =
-        await response.json();
-
-        document.getElementById(
-        "productImage"
-        ).src = data.thumbnail;
-
-        document.getElementById(
-        "productName"
-        ).innerText = data.title;
-
-        document.getElementById(
-        "productPrice"
-        ).innerText = "₹" + data.price;
-
-    }
-    catch(error){
-
-        console.log(error);
-
-    }
-
-}
-
-loadProduct();
-
-
-
-
 const SHIPPING = 10;
 const TAX_RATE = 0.05;
 
@@ -277,6 +238,3 @@ document.addEventListener("DOMContentLoaded", () => {
     updateCartCount();
     initializeOrderButton();
 });
-
-
-
