a3a3cd0abd0740b7ec16e376ca40c2f1a4bbaf02
Aman Kumar <amanthecoder001@gmail.com>
2026-06-12 14:36:27 +0530

    added html part for handling of no news present

--- Project_03-Aman-Bhumika/jsFiles/app.js
+++ Project_03-Aman-Bhumika/jsFiles/app.js
@@ -20,7 +20,7 @@ async function getNews(category) {
         if (newsData.articles.length === 0) {
             throw new Error("No Articles Available");
         }
-  console.log(newsData.articles);
+        console.log(newsData.articles);
         return newsData.articles;
 
     }
@@ -47,7 +47,16 @@ function renderNewsCards(articles) {
 
     container.innerHTML = "";
 
+
+
     articles.slice(0, 6).forEach(article => {
+       const date = new Date(article.publishedAt);
+
+        const formattedDate = date.toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric"
+        });
 
         container.innerHTML += `
             <div class="news-card">
@@ -61,7 +70,7 @@ function renderNewsCards(articles) {
                 </div>
                 <hr class="news-hr">
                     <div class="news-card-bottom">
-                        <p id="date">${new Date(article.publishedAt).toLocaleDateString("en-GB")}</p>
+                        <p id="date">${formattedDate}</p>
                     <a hjref= "${article.url}">Read more</a>
                     </div>
 
@@ -73,40 +82,40 @@ function renderNewsCards(articles) {
 }
 
 
-        async function getWeather(){
-            const lat = 28.57;
-            const lon = 77.55;
-            // const API_KEY = "fd93ea356d0c60b7649b41d419e306ed";
+async function getWeather() {
+    const lat = 28.57;
+    const lon = 77.55;
+    // const API_KEY = "fd93ea356d0c60b7649b41d419e306ed";
 
-            const cityName = document.getElementById("city-name");
-            const temp = document.getElementById("temp");
-            const condition = document.getElementById("condition");
-            const humidity = document.getElementById("humidity");
-            const wind = document.getElementById("wind");
-            const icon = document.getElementById("weather-icon");
-            try{
-                console.log(API_KEY);
-                const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
-                if(!response.ok){
-                throw new Error("API Error");
-            }
-                const data = await response.json();
-                console.log(data);
-                const weather = data.list[0];
-                cityName.textContent = data.city.name;
-                temp.textContent = (weather.main.temp) + " °C";
-                condition.textContent = weather.weather[0].description;
-                humidity.textContent = weather.main.humidity + " %";
-                wind.textContent = weather.wind.speed + " m/s";
-                const iconCode = weather.weather[0].icon;
-                icon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-                
-
-
-            }
-            catch(error){
-                console.log(error);
-            }
-            
+    const cityName = document.getElementById("city-name");
+    const temp = document.getElementById("temp");
+    const condition = document.getElementById("condition");
+    const humidity = document.getElementById("humidity");
+    const wind = document.getElementById("wind");
+    const icon = document.getElementById("weather-icon");
+    try {
+        console.log(API_KEY);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+        if (!response.ok) {
+            throw new Error("API Error");
         }
-        getWeather();
+        const data = await response.json();
+        console.log(data);
+        const weather = data.list[0];
+        cityName.textContent = data.city.name;
+        temp.textContent = (weather.main.temp) + " °C";
+        condition.textContent = weather.weather[0].description;
+        humidity.textContent = weather.main.humidity + " %";
+        wind.textContent = weather.wind.speed + " m/s";
+        const iconCode = weather.weather[0].icon;
+        icon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+getWeather();
