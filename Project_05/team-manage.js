7bc252bec4352eed9e3c124f6a144ffcd5410c22
Aman Kumar <amanthecoder001@gmail.com>
2026-07-17 14:59:04 +0530

    implement logic at btn click for saving data and rendering data

--- Project_05/team-manage.js
+++ Project_05/team-manage.js
@@ -12,3 +12,35 @@ function formatDate(dateString) {
     };
     return new Date(dateString).toLocaleDateString("en-GB", options);
 }
+
+document.getElementById("add-member-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (fullName.value == "" || emailId.value == "" || role.value == "") {
+        alert("Fill all the details");
+        return;
+    }
+
+    let members = JSON.parse(localStorage.getItem("members")) || [];
+
+    const member = {
+        id: Date.now(),
+        name: fullName.value,
+        email: emailId.value,
+        role: role.value,
+        joinedOn: formatDate(new Date()),
+        avatar: avatar.value,
+        taskCount: 0
+    };
+
+    members.push(member);
+
+    saveMembers(members);
+
+    renderMembers();
+
+    fullName.value = "";
+    emailId.value = "";
+    role.value = "";
+    avatar.value = "";
+})
