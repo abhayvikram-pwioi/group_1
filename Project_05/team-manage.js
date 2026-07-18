8957f6089cad4eefc2402bc3f0e78f0d7c8db2d3
Aman Kumar <amanthecoder001@gmail.com>
2026-07-18 13:44:19 +0530

    function for updating team summary

--- Project_05/team-manage.js
+++ Project_05/team-manage.js
@@ -176,3 +176,39 @@ document.getElementById("task-form").addEventListener("submit", function (e) {
 function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+
+
+function updateSummary() {
+
+    const members = JSON.parse(localStorage.getItem("members")) || [];
+
+    document.getElementById("total-member").textContent = members.length;
+
+    document.getElementById("total-ui-ux").textContent =
+        members.filter(member => member.role === "UI/UX Designer").length;
+
+    document.getElementById("total-frontend").textContent =
+        members.filter(member => member.role === "Frontend Developer").length;
+
+    document.getElementById("total-backend").textContent =
+        members.filter(member => member.role === "Backend Developer").length;
+
+    document.getElementById("total-project-manager").textContent =
+        members.filter(member => member.role === "Project Manager").length;
+
+    document.getElementById("total-devops").textContent =
+        members.filter(member => member.role === "DevOps Engineer").length;
+
+    document.getElementById("total-testers").textContent =
+        members.filter(member => member.role === "QA Tester").length;
+
+}
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    renderMembers();
+    loadMembers();
+    updateSummary();
+});
