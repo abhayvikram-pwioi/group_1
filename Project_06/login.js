164a7b2916ebbaa265d9d57c8824f9b4269bd32f
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-07-23 13:26:59 +0530

    Implement login functionality with error handling

--- Project_06/login.js
+++ Project_06/login.js
@@ -6,3 +6,31 @@ const loginForm = document.getElementById("loginForm");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const remember = document.getElementById("remember");
+
+loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const userEmail = email.value;
+    const userPassword = password.value;
+
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, userEmail, userPassword);
+        alert("Login Successful");
+        console.log(userCredential.user);
+
+        window.location.href = "dashboard.html";
+    } catch (error) {
+
+        if (error.code === "auth/invalid-credential") {
+            alert("Invalid email or password.");
+
+        } else if (error.code === "auth/invalid-email") {
+            alert("Please enter a valid email.");
+        } else {
+
+            alert(error.message);
+
+        }
+
+    }
+
+});
