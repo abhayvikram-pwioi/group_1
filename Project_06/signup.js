3a6e3f1c43881d8650e9f0782e97cbf5469bc390
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-07-23 13:25:06 +0530

    Initialize signup form elements
    Added form elements for user signup.

--- Project_06/signup.js
+++ Project_06/signup.js
@@ -1,1 +1,10 @@
+import { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+const signupForm = document.getElementById("signupForm");
+const fullName = document.getElementById("fullName");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const terms = document.getElementById("terms");
