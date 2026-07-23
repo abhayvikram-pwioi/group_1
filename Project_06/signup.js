3679f3c32512f3b0c303367ee0d119d9674e2228
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-07-23 13:25:35 +0530

    Implement signup form validation
    Added validation for user input during signup process.

--- Project_06/signup.js
+++ Project_06/signup.js
@@ -8,3 +8,31 @@ const phone = document.getElementById("phone");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
 const terms = document.getElementById("terms");
+
+signupForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const userName = fullName.value;
+    const userEmail = email.value;
+    const userPhone = phone.value;
+    const userPassword = password.value;
+    const userConfirmPassword = confirmPassword.value;
+
+    if (userName.trim() === "") {
+        alert("Please enter your name.");
+        return;
+    }
+
+    if (userPassword !== userConfirmPassword) {
+        alert("Passwords do not match");
+        return;
+    }
+
+    if (userPhone.length !== 10) {
+        alert("Phone number must be 10 digits.");
+        return;
+    }
+
+    if (!terms.checked) {
+        alert("Please accept the Terms & Conditions.");
+        return;
+    }
