ff6121008118ea9761243355ddcf5d8c54a7d4f3
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-07-23 13:26:44 +0530

    Add Firebase authentication imports to login.js

--- Project_06/login.js
+++ Project_06/login.js
@@ -1,1 +1,8 @@
+import { auth } from "./firebase.js";
 
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const loginForm = document.getElementById("loginForm");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const remember = document.getElementById("remember");
