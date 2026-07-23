6b009ed3b0506c37db4c53d397cf951cf9678fad
Aman-kumar <98403770+Aman-kumar-2007@users.noreply.github.com>
2026-07-23 13:23:35 +0530

    Initialize Firebase app and authentication

--- Project_06/firebase.js
+++ Project_06/firebase.js
@@ -1,3 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdHuhYp9tZ-iGMTyPCvqzNZmqmy-5A5iQ",
+  authDomain: "student-progress-tracker-fe22f.firebaseapp.com",
+  projectId: "student-progress-tracker-fe22f",
+  storageBucket: "student-progress-tracker-fe22f.firebasestorage.app",
+  messagingSenderId: "384667192167",
+  appId: "1:384667192167:web:1ce587623e800ec2c73dc2",
+  measurementId: "G-V9B2ELWW6Z"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { auth };
